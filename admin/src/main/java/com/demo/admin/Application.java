54b8351ab1e78df7e98cb7a1b51c0838e8dcfd41
Jonkey Guan <jonkey.guan@gmail.com>
2021-03-09 00:44:24 +0800

    fixed admin

--- admin/src/main/java/com/demo/admin/Application.java
+++ admin/src/main/java/com/demo/admin/Application.java
@@ -2,6 +2,7 @@ package com.demo.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableAdminServer
 public class Application {
 
